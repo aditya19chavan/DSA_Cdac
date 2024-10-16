a9af5787322228b102bb74184ffdf23fa9aa568f
aditya19chavan <155563749+aditya19chavan@users.noreply.github.com>
2024-10-16 20:35:25 +0530

    Update Circular_doublyLL.java

--- LinkedList/Circular_doublyLL.java
+++ LinkedList/Circular_doublyLL.java
@@ -11,15 +11,17 @@ public class Circular_doublyLL {
 		Dnode n = new Dnode(data);
 		if (root == null) {
 			root = n;// n becomes 1st so root
+			root.left=root;
+			root.right=root;
+			root.last=root;
 		} else {
-			n.last = root;
-			n.right = root;// 1
-			root.left = n; // 2
-			root = n;// 3
+	                n.left=root.left;
+			n.right = root;
+			//root.left.right = n; 
+			root.left = n;
 			root.last =n;
-            root.right=null;
-			root.left = null;
-			root.right = root;
+           
+			
 
 		}
 		System.out.println(root.data + " inserted");
@@ -29,16 +31,16 @@ public class Circular_doublyLL {
 	void insert_right(int data) {
 		Dnode n = new Dnode(data);
 		if (root == null)
-			root = n;// n becomes 1st so root=
+			root = n;// n becomes 1st so root
+			root.left=root;
+		        root.right=root;
+		        root.last=root;
 		else {
-			Dnode t = root;
-			root.left=t;
-			while (t.right != null) {
-				t = t.right;
-			}
-			
-			t.right = n;
-			n.left = t;
+	                n.left=root;
+			n.right=root.right;
+			//root.right.left=n;
+			root.right=n;
+			root.last=n;
 		
 			
 		}
@@ -50,13 +52,11 @@ public class Circular_doublyLL {
 		if (root == null) {
 			System.out.println("Empty List");
 		} else {
-			Dnode t;
+			Dnode t =root.left;
 			t = root;// 1
-			root = root.right;// 2
-			root.left = null;
-			root.last=root;
-			t.right=root;
-			root.left=t;
+			root.left = t.left;// 2
+			t.left.right=t.left;
+			root.last=t.left;
 			
 			System.out.println(t.data + " deleted");
 		}
@@ -64,24 +64,16 @@ public class Circular_doublyLL {
 	
 	
 	void delete_right() {
-		if (root == null)
-			System.out.println("Empty List");
-		else {
-			Dnode t, t2;
-			t = root;// 1
-			while (t.right != null)
-				t = t.right;
-
-			if (t == root)// single node
- 			root = null;// reset root as only node left
-			else {
-				t2 = t.left;// 3
-				t2.right = root;// 4
-				root.right=t2;
-			}
-			System.out.println(t.data + " deleted");
-		}
-	}
+		 if (root == null) {
+            System.out.println("Empty List");
+        } else {
+            Dnode t = root.right;
+            root.right = t.right;
+            t.right.left = root;
+            root.last = t.right;
+            System.out.println(t.data + " deleted");
+        }
+    }
 	
 	
 	void print_list() {
@@ -92,7 +84,20 @@ public class Circular_doublyLL {
 		
 				System.out.print("<-|" + t.data + "|->");
 				t = t.right;
-			
+			 } while (t != root);
+            System.out.println();
+        }
+    }
+ void printReverse() {
+        if (root == null) {
+            System.out.println("List Empty");
+        } else {
+            Dnode t = root.last;
+            do {
+                System.out.print("<-|" + t.data + "|->");
+                t = t.left;
+            } while (t != root.last);
+            System.out.println();
 			
 		}
 	}
